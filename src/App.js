9bd955eae064595daf1b45731bc9729c9d5e42ed
Justin <justinrkinsman@gmail.com>
2022-08-19 10:32:24 -0300

    Convert exisiting components to class components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { Component } from "react"
 
+class App extends Component {
+  constructor(){
+    super()
 
-function App() {
-  return (
-    console.log('hello')
-  );
+    /*this.state goes here*/
+  }
+
+  /*functions go here */
+
+  render() {
+    let hi = console.log('hello')
+    return hi
+  }
 }
 
 export default App;
