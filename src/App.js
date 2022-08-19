5e093603aabe08ff4fc63f4413a273b32b286b2c
Justin <justinrkinsman@gmail.com>
2022-08-19 12:14:56 -0300

    Add function to App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ export class App extends Component {
     /*this.state goes here*/
   }
 
-  /*functions go here */
+  generalInfoSubmission = (e) => {
+    e.preventDefault()
+  }
 
   render() {
     return (
@@ -20,18 +22,21 @@ export class App extends Component {
               <legend>General Information</legend>
               <form id="generalInfo">
                 <div id="nameForm">
-                  <label htmlFor="nameInput">Name: </label>
-                  <input type="text" name="nameInput" id="nameInput"></input>
+                  <label htmlFor="nameInput">Name:
+                    <input type="text" name="nameInput" id="nameInput"></input>
+                  </label>                
                 </div>
                 <div id="emailForm">
-                  <label htmlFor="emailInput">Email: </label>
-                  <input type="text" name="emailInput" id="emailInput"></input>
+                  <label htmlFor="emailInput">Email: 
+                    <input type="text" name="emailInput" id="emailInput"></input>
+                  </label>
                 </div>
                 <div id="phoneForm">
-                  <label htmlFor="phoneInput">Phone: </label>
-                  <input type="text" name="phoneInput" id="phoneInput"></input>
+                  <label htmlFor="phoneInput">Phone: 
+                    <input type="text" name="phoneInput" id="phoneInput"></input>
+                  </label>
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={this.generalInfoSubmission}>Submit</button>
                 <button type="edit" className="invisible" >Edit</button>
               </form>
             </fieldset>
@@ -39,16 +44,19 @@ export class App extends Component {
               <legend>Education</legend>
               <form>
                 <div id="schoolForm">
-                  <label htmlFor="schoolInput">School: </label>
-                  <input type="text" name="schoolInput" id="schoolInput"></input>
+                  <label htmlFor="schoolInput">School: 
+                    <input type="text" name="schoolInput" id="schoolInput"></input>
+                  </label>
                 </div>
                 <div id="programForm">
-                  <label htmlFor="programInput">Area of Study: </label>
-                  <input type="text" name="programInput" id="programInput"></input>
+                  <label htmlFor="programInput">Area of Study:                 
+                    <input type="text" name="programInput" id="programInput"></input>
+                  </label>
                 </div>
                 <div id="yearForm">
-                  <label htmlFor="yearInput">Year of Study: </label>
-                  <input type="text" name="yearInput" id="yearInput"></input>
+                  <label htmlFor="yearInput">Year of Study: 
+                    <input type="text" name="yearInput" id="yearInput"></input>
+                  </label>
                 </div>
                 <button type="submit">Submit</button>
                 <button type="edit" className="invisible" >Edit</button>
@@ -58,20 +66,24 @@ export class App extends Component {
               <legend>Practical Experience</legend>
               <form>
                 <div id="companyForm">
-                  <label htmlFor="companyInput">Company: </label>
-                  <input type="text" name="companyInput" id="companyInput"></input>
+                  <label htmlFor="companyInput">Company: 
+                    <input type="text" name="companyInput" id="companyInput"></input>
+                  </label>                
                 </div>
                 <div id="titleForm">
-                  <label htmlFor="titleInput">Job Title: </label>
-                  <input type="text" name="titleInput" id="titleInput"></input>
+                  <label htmlFor="titleInput">Job Title: 
+                    <input type="text" name="titleInput" id="titleInput"></input>
+                  </label>                
                 </div>
                 <div id="tasksForm">
-                  <label htmlFor="tasksInput">Job Tasks: </label>
-                  <input type="text" name="tasksInput" id="tasksInput"></input>
+                  <label htmlFor="tasksInput">Job Tasks: 
+                    <input type="text" name="tasksInput" id="tasksInput"></input>
+                  </label>                
                 </div>
                 <div id="dateForm">
-                  <label htmlFor="dateInput">Years Employed: </label>
-                  <input type="text" name="dateInput" id="dateInput"></input>
+                  <label htmlFor="dateInput">Years Employed: 
+                    <input type="text" name="dateInput" id="dateInput"></input>
+                  </label>                
                 </div>
                 <button type="submit">Submit</button>
                 <button type="edit" className="invisible" >Edit</button>
